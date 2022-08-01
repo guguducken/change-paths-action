a395c273e80e60f5e830ae468b15c6322676ad37
guguducken <1173781832@qq.com>
2022-08-01 21:28:55 +0800

    ignore rule bug fix

--- src/index.js
+++ src/index.js
@@ -199,7 +199,7 @@ async function getIgnorePathRe(str) {
     }
 
     for (const it of ans_re) {
-        core.info(it.re.toString(), " ", it.fullIgnore);
+        core.info(it.re.toString() + it.fullIgnore);
     }
     return ans_re;
 }
